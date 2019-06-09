ef7540f130fe60688eda5f809208cf57d95650f8
Williams Mendez <wmendez27@gmail.com>
2019-06-09 11:21:53 -0400

    Test order of tweets

--- apps/twitter/tests.py
+++ apps/twitter/tests.py
@@ -6,6 +6,7 @@ from apps.twitter.models import Tweet
 
 class TweetTestCase(TestCase):
     def test_post_tweet(self):
+        """Test tweet insertion through DRF endpoint"""
         data = {
             'name': 'john doe',
             'message': 'This is a test'
@@ -17,4 +18,21 @@ class TweetTestCase(TestCase):
         self.assertEqual(data['name'], result['name'])
         self.assertEqual(data['message'], result['message'])
         self.assertEqual(1, Tweet.objects.count())
-        print(result)
+
+    def test_tweets_list(self):
+        """Test list of tweets ordered by date of creation LIFO"""
+        for i in range(1, 4):
+            Tweet.objects.create(
+                name=f'User {i}',
+                message=f'Message from user {1}'
+            )
+
+        response = self.client.get(reverse('tweet-list'))
+        self.assertEqual(200, response.status_code)
+
+        result = response.json()
+        names = [tweet['name'] for tweet in result]
+
+        # Expect the same names in reverse order
+        expected = [f'User {i}' for i in reversed(range(1, 4))]
+        self.assertEqual(expected, names)
