487a364dc2dd500d809986ae3723d5766e69659b
Williams Mendez <wmendez27@gmail.com>
2019-06-09 10:48:03 -0400

    Bootstrap django project

---
+++ apps/twitter/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Tweet(models.Model):
+    name = models.CharField(max_length=32)
+    message = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
